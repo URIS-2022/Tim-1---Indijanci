1fcbd71812cdad4fedbc96f22c9f572c360a1a19
Maja Jukic <mjukic2000@gmail.com>
2022-11-09 20:23:25 +0100

    S1125, S2386, S1481, S101, S2486 bug fix

--- src/Api/Controllers/CiphersController.cs
+++ src/Api/Controllers/CiphersController.cs
@@ -103,8 +103,9 @@ public class CiphersController : Controller
     {
         var userId = _userService.GetProperUserId(User).Value;
         var hasOrgs = _currentContext.Organizations?.Any() ?? false;
+        var withOrg = true || hasOrgs;
         // TODO: Use hasOrgs proper for cipher listing here?
-        var ciphers = await _cipherRepository.GetManyByUserIdAsync(userId, true || hasOrgs);
+        var ciphers = await _cipherRepository.GetManyByUserIdAsync(userId, withOrg);
         Dictionary<Guid, IGrouping<Guid, CollectionCipher>> collectionCiphersGroupDict = null;
         if (hasOrgs)
         {
